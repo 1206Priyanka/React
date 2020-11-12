56134dbd367bde15c52b8741e100911d329dac9d
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2020-11-12 19:32:07 +0000

    Create productApp.js

---
+++ productApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Product from './Product';
+
+function Products(){
+
+return (
+
+    <div>
+
+        <Product id = "P1" name = "car"/>
+        <Product id = "P2" name = "compiter"/>
+
+    </div>
+
+);
+}
+
+export default Products;
