b7c7ac9f6d60f7fa669fa93fa80ebf283f9802d8
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2020-12-10 13:08:21 +0000

    Update Item.js

--- Item.js
+++ Item.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 function Item (props){
 
-    const itemId = props.itemId;
-    const itemName = props.itemName;
+    //hooks
+    //const itemId = props.itemId;
+    //const itemName = props.itemName;
+    
+    //setters
+    const [itemName, setItemName]= React.useState(props.itemName);
+    const[itemId, setItemId]= React.useState(props.itemId);
+    
+    //Event Handler
+    function clickHandler(){
+      setItemName("Apple");
+      setItemId("Apple1");
+      window.alert("This is an item with name given as = " +itemName);
+    }
 
 
 
     return(
         <div>
             <h1>Hello from Item. The id is = {itemId}. The name is {itemName}</h1>
+            <input type ="text" placeholder ="Item" value ={itemName}></input>
+            <br></br>
+
+            <button onClick = {clickHandler}>Show Item Details</button>
     
         </div>
     );
